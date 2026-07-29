b21220962fa6bb870325e840b46332d7245463d8
VTS <vannestheo@gmail.com>
2026-07-29 15:34:47 +0700

    redesign first init

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,9 @@ import { useState } from "react";
 import "bootstrap-icons/font/bootstrap-icons.css";
 
 import Home from "./pages/Home";
-import About from "./pages/About";
-import Shop from "./pages/Shop";
-import Blog from "./pages/Blog";
-import ArticleContent from "./pages/ArticleContent";
-import ContactUs from "./pages/ContactUs";
-import Cart from "./pages/Cart";
 
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
+// import Navbar from "./components/Navbar";
+// import Footer from "./components/Footer";
 
 function ScrollToTop() {
   const { pathname } = useLocation();
@@ -30,47 +24,19 @@ function ScrollToTop() {
 }
 
 export default function App() {
-  const [cart, setCart] = useState([]);
-
-  const addToCart = (item) => {
-    setCart(prevCart => [...prevCart, item]);
-  }
-
-  const removeFromCart = (id) => {
-    setCart(prevCart => prevCart.filter(item => item.product.id !== id));
-  };
-
-  const updateQty = (id, newQty) => {
-    if (newQty <= 0) return;
-
-    setCart(prev =>
-      prev.map(item =>
-        item.product.id === id
-          ? { ...item, qty: newQty }
-          : item
-      )
-    );
-  }
-
   return (
     <Router>
       <ScrollToTop />
 
-      <Navbar cart={cart} />
+      {/* <Navbar cart={cart} /> */}
 
       <main className="bg-black text-white">
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/about-us" element={<About />} />
-          <Route path="/shop" element={<Shop cart={cart} addToCart={addToCart} />} />
-          <Route path="/blog" element={<Blog />} />
-          <Route path="/blog/:slug" element={<ArticleContent />} />
-          <Route path="/contact-us" element={<ContactUs />} />
-          <Route path="/cart" element={<Cart cart={cart} removeFromCart={removeFromCart} updateQty={updateQty} />} />
         </Routes>
       </main>
 
-      <Footer />
+      {/* <Footer /> */}
     </Router>
   );
 }
